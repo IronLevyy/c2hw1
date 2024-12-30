20e0ed99a9f6683d1de748537505c03678c1f81b
Iron Levyy <olegan.plav@gmail.com>
2024-12-30 12:13:24 +0300

    homework4 corrections done

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -13,15 +13,46 @@ import java.util.ArrayList;
 public class App {
     private static SearchEngine searchEngine = new SearchEngine();
     public static void main(String[] args) {
-
-            createSimpleProduct("",70);
-            createSimpleProduct("Яйца", -120);
-            createFixPriceProduct("Молоко");
-            createDiscountProduct("Молоко",200,150);
-            createSimpleProduct("Банан",90);
-            createArticle("Первая статья", "Текст");
-            createArticle("Первая статья", "Это моя Первая статья");
-            createArticle("Вторая статья", "Текст");
+        try {
+            safelyAddToSearchEngine(searchEngine,new SimpleProduct("",70));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
+        try {
+            safelyAddToSearchEngine (searchEngine,new SimpleProduct("Яйца", -120));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
+        try {
+            safelyAddToSearchEngine (searchEngine, new FixPriceProduct("Молоко"));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
+        try {
+            safelyAddToSearchEngine (searchEngine, new DiscountProduct("Молоко",200,150));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
+        try {
+            safelyAddToSearchEngine (searchEngine, new SimpleProduct("Банан",90));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
+        try {
+            safelyAddToSearchEngine (searchEngine, new Article("Первая статья", "Текст"));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
+        try {
+            safelyAddToSearchEngine (searchEngine, new Article("Первая статья", "Это моя Первая статья"));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
+        try {
+            safelyAddToSearchEngine (searchEngine, new Article("Вторая статья", "Текст"));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
 
         ArrayList<Searchable> searchOne = searchEngine.search("Первая статья Текст");
         ArrayList<Searchable> searchTwo = searchEngine.search("Молоко");
@@ -48,36 +79,7 @@ public class App {
         }
     }
 
-    private static void createSimpleProduct(String name, int price) {
-        try {
-            SimpleProduct product = new SimpleProduct(name, price);
-            searchEngine.addObjectToSearchList(product);
-        } catch (Exception e) {
-            System.err.println("Ошибка " + e.getMessage());
-        }
-    }
-    private static void createDiscountProduct(String name, int price, int discount) {
-        try {
-            DiscountProduct product = new DiscountProduct(name, price, discount);
-            searchEngine.addObjectToSearchList(product);
-        } catch (Exception e) {
-            System.err.println("Ошибка " + e.getMessage());
-        }
-    }
-    private static void createFixPriceProduct(String name) {
-        try {
-            FixPriceProduct product = new FixPriceProduct(name);
-            searchEngine.addObjectToSearchList(product);
-        } catch (Exception e) {
-            System.err.println("Ошибка " + e.getMessage());
-        }
-    }
-    private static void createArticle(String title, String body) {
-        try {
-            Article article = new Article(title, body);
-            searchEngine.addObjectToSearchList(article);
-        } catch (Exception e) {
-            System.err.println("Ошибка " + e.getMessage());
-        }
+    private static void safelyAddToSearchEngine(SearchEngine searchEngine, Searchable searchable) {
+            searchEngine.addObjectToSearchList(searchable);
     }
 }
