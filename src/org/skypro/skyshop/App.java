e1d08ae7e43338856519819055eade39c63fcf04
Iron Levyy <olegan.plav@gmail.com>
2024-12-30 03:13:23 +0300

    homework4 corrections done

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -11,56 +11,17 @@ import org.skypro.skyshop.search.Searchable;
 import java.util.ArrayList;
 
 public class App {
+    private static SearchEngine searchEngine = new SearchEngine();
     public static void main(String[] args) {
-        SearchEngine searchEngine = new SearchEngine();
-        try {
-            SimpleProduct milk = milk = new SimpleProduct(" ", 70);
-            searchEngine.addObjectToSearchList(milk);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-        try {
-            SimpleProduct eggs = new SimpleProduct("Яйца", -120);
-            searchEngine.addObjectToSearchList(eggs);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-        try {
-            FixPriceProduct bread = new FixPriceProduct("Молоко");
-            searchEngine.addObjectToSearchList(bread);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-        try {
-            DiscountProduct butter = new DiscountProduct("Молоко", 200, 150);
-            searchEngine.addObjectToSearchList(butter);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-        try {
-            SimpleProduct banana = new SimpleProduct("Бананы", 90);
-            searchEngine.addObjectToSearchList(banana);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-        try {
-            Article article = new Article("Первая статья", "Текст");
-            searchEngine.addObjectToSearchList(article);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-        try {
-            Article article = new Article("Первая статья", "Это моя Первая статья.");
-            searchEngine.addObjectToSearchList(article);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-        try {
-            Article article = new Article("Вторая статья", "Текст");
-            searchEngine.addObjectToSearchList(article);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
+
+            createSimpleProduct("",70);
+            createSimpleProduct("Яйца", -120);
+            createFixPriceProduct("Молоко");
+            createDiscountProduct("Молоко",200,150);
+            createSimpleProduct("Банан",90);
+            createArticle("Первая статья", "Текст");
+            createArticle("Первая статья", "Это моя Первая статья");
+            createArticle("Вторая статья", "Текст");
 
         ArrayList<Searchable> searchOne = searchEngine.search("Первая статья Текст");
         ArrayList<Searchable> searchTwo = searchEngine.search("Молоко");
@@ -69,13 +30,13 @@ public class App {
             Searchable searchTestOne = searchEngine.getMostSuitable("Первая статья");
             System.out.println(searchTestOne);
         } catch (BestResultNotFound e) {
-            e.printStackTrace();
+            System.err.println("Ошибка " + e.getMessage());
         }
         try {
             Searchable searchTestTwo = searchEngine.getMostSuitable("Инкогнито");
             System.out.println(searchTestTwo);
         } catch (BestResultNotFound e) {
-            e.printStackTrace();
+            System.err.println("Ошибка " + e.getMessage());
         }
 
         for (Searchable searchable : searchOne) {
@@ -84,6 +45,39 @@ public class App {
 
         for (Searchable searchable : searchTwo) {
             searchable.getStringRepresentation();
+        }
+    }
+
+    private static void createSimpleProduct(String name, int price) {
+        try {
+            SimpleProduct product = new SimpleProduct(name, price);
+            searchEngine.addObjectToSearchList(product);
+        } catch (Exception e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
+    }
+    private static void createDiscountProduct(String name, int price, int discount) {
+        try {
+            DiscountProduct product = new DiscountProduct(name, price, discount);
+            searchEngine.addObjectToSearchList(product);
+        } catch (Exception e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
+    }
+    private static void createFixPriceProduct(String name) {
+        try {
+            FixPriceProduct product = new FixPriceProduct(name);
+            searchEngine.addObjectToSearchList(product);
+        } catch (Exception e) {
+            System.err.println("Ошибка " + e.getMessage());
+        }
+    }
+    private static void createArticle(String title, String body) {
+        try {
+            Article article = new Article(title, body);
+            searchEngine.addObjectToSearchList(article);
+        } catch (Exception e) {
+            System.err.println("Ошибка " + e.getMessage());
         }
     }
 }
